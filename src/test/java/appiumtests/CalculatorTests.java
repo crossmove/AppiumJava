670e7ed8d7a272046762a088197abc5e03bc35c4
pradeep <pradeep.exe@gmail.com>
2020-04-19 14:54:17 +0400

    first commit

---
+++ src/test/java/appiumtests/CalculatorTests.java
@@ -0,0 +1,31 @@
+package appiumtests;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class CalculatorTests {
+
+	static AppiumDriver<MobileElement> appDriver;
+	
+	public static void main(String[] args) throws Exception {
+		
+		OpenCalculator();
+
+	}	
+	public static void OpenCalculator() throws Exception {				
+		DesiredCapabilities cap = new DesiredCapabilities();		
+		cap.setCapability("deviceName", "vivo 1920");
+		cap.setCapability("udid", "575b6cf1");
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("platformVersion", "9");		
+		cap.setCapability("appPackage", "com.android.bbkcalculator");
+		cap.setCapability("appActivity", "com.android.bbkcalculator.Calculator");		
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");		
+		appDriver = new AppiumDriver<MobileElement>(url,cap);		
+		System.out.print("Starting Calculator app");
+	}
+
+}
